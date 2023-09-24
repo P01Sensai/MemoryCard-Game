f7567fff949fc192e1cf46bf8a4a34f9223b19a8
P01Sensai <pramanshuprajapati@gmil.com>
2023-09-24 19:37:43 +0530

    updated the splash screen timing

--- lib/animatedSplash.dart
+++ lib/animatedSplash.dart
@@ -2,6 +2,7 @@ import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 
 import 'main.dart';
+import 'start.dart';
 
 
 class SplashScreen extends StatelessWidget {
@@ -10,38 +11,38 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AnimatedSplashScreen(
-      splash: const SingleChildScrollView(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: 100,
-                width: 100,
-                child: ClipOval(
-                  child: Image(
-                    image: NetworkImage('https://picsum.photos/100/100'),
-                    fit: BoxFit.cover,
-                  ),
+      splash: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 100,
+              width: 100,
+              child: ClipOval(
+                child: Image(
+                  image: NetworkImage('https://picsum.photos/100/100'),
+                  fit: BoxFit.cover,
                 ),
               ),
-              // SizedBox(height: 20),
-              Text(
-                'Splash Screen testing',
-                overflow: TextOverflow.ellipsis,
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Splash Screen testing',
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
-      nextScreen: const MemoryCardGame(),
+      nextScreen: const start(),
       splashTransition: SplashTransition.fadeTransition,
-      duration: 30000,
+     // pageTransitionType: ,
+      duration: 3000,
       backgroundColor: Colors.white,
+
     );
   }
 }
