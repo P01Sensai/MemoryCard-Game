7ddaf32d1fe8b8729b0c17435fcf4ee726d92725
P01Sensai <pramanshuprajapati@gmil.com>
2023-09-28 21:38:29 +0530

    start developing the start page properly changes 3  added few structure uses decoration in name conatiner

--- lib/newstart.dart
+++ lib/newstart.dart
@@ -20,23 +20,58 @@ class _startState extends State<newstart> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
       appBar: AppBar(
-        title: const Center(child:  Text('MemoryCard Game')),
-      ) ,
-      body: Container(
-        padding: const EdgeInsets.all(20),
-        margin: const EdgeInsets.fromLTRB(40, 10, 50, 40),
-        color: Colors.lightGreen,
-        child: const  Text( 'Welcome to MemoryCard Game',style: TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.bold, color: Colors.white,)
+        title: const Center(child: Text('MemoryCard Game',style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+          color: Colors.purple),)),
+      ),
 
-        ),
+      body: Column(
+        //mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const SizedBox(height: 10,),
+          Container(
+            padding: const EdgeInsets.all(20),
+            margin: const EdgeInsets.fromLTRB(40, 10, 50, 40),
+            //color: Colors.lightGreen,
+            decoration: BoxDecoration(
+              border: Border.all(width: 2,color: Colors.transparent),
+              borderRadius: const BorderRadius.all( Radius.circular(10)) ,
+              color: Colors.black26,
+            ),
 
+            child: const Text(
+              'Welcome to MemoryCard Game',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          const SizedBox(height: 20,),
+          Container(
+            padding: const EdgeInsets.all(20),
+            margin: const EdgeInsets.fromLTRB(40, 10, 50, 40),
+            //color: Colors.blue,
 
+            child: const Text(
+              'Choose Difficulty Level',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
 
+          ),
 
-      ));
-
+          // Add more containers here
+        ],
+      ),
+    );
   }
+
 }
