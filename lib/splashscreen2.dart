75629c9e800c6003b8992311fa6f4807d6ce6407
P01Sensai <pramanshuprajapati@gmil.com>
2023-10-01 13:48:59 +0530

    chnages in splashscreen2.dart changes 8 changed background colour

--- lib/splashscreen2.dart
+++ lib/splashscreen2.dart
@@ -1,3 +1,5 @@
+
+
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:page_transition/page_transition.dart';
@@ -16,11 +18,49 @@ class SScreen2 extends StatelessWidget {
     return AnimatedSplashScreen(
       splash:  Image.asset('assets/milad-fakurian-PGdW_bHDbpI-unsplash.jpg'),
       nextScreen: const start(),
-      splashTransition: SplashTransition.sizeTransition,
+      splashTransition: SplashTransition.decoratedBoxTransition,
       pageTransitionType: PageTransitionType.fade,
-      duration: 3000,
+      duration: 1000,
       backgroundColor: Colors.purpleAccent,
+
 
     );
   }
 }
+
+// class SScreen2 extends StatelessWidget {
+//   const SScreen2({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return AnimatedSplashScreen(
+//       splash: FutureBuilder<Image>(
+//         future: loadImage('assets/milad-fakurian-PGdW_bHDbpI-unsplash.jpg'),
+//         builder: (context, snapshot) {
+//           if (snapshot.connectionState == ConnectionState.waiting) {
+//             return Center(child: CircularProgressIndicator());
+//           } else if (snapshot.hasError) {
+//             // Handle image loading error
+//             return Center(child: Text('Error loading splash screen'));
+//           } else if (snapshot.hasData) {
+//             return snapshot.data!;
+//           } else {
+//             return Center(child: Text('No Data'));
+//           }
+//         },
+//       ),
+//       nextScreen: const start(),
+//       splashTransition: SplashTransition.sizeTransition,
+//       pageTransitionType: PageTransitionType.fade,
+//       duration: 3000,
+//       backgroundColor: Colors.purpleAccent,
+//     );
+//   }
+//
+//   Future<Image> loadImage(String path) async {
+//     final image = Image.asset(path);
+//     await precacheImage(image.image, context as BuildContext);
+//     return image;
+//   }
+// }
+
