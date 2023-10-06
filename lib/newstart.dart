916cce77c725719059a663064a5b6cca89604059
P01Sensai <pramanshuprajapati@gmil.com>
2023-10-06 11:08:41 +0530

    start changes

--- lib/newstart.dart
+++ lib/newstart.dart
@@ -140,14 +140,14 @@ class _startState extends State<newstart> {
                 border: Border.all(width: 1,color: Colors.white),
                 borderRadius: const BorderRadius.all( Radius.circular(10)) ,
                 color: Colors.amber,
-                // boxShadow: const [
-                //   BoxShadow(
-                //     color: Colors.white54,
-                //     blurRadius: 10,
-                //     spreadRadius: 1,
-                //     offset: Offset(0,4),
-                //   )
-                //]
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.lightBlueAccent,
+                    blurRadius: 10,
+                    spreadRadius: 1,
+                    offset: Offset(0,4),
+                  )
+                ]
             ),
 
             child: Center(
