485197daf9c2ec7666b9484dd62c2c27cfd60b8e
P01Sensai <pramanshuprajapati@gmil.com>
2023-10-14 23:18:09 +0530

    addedd the hard lvl changes

---
+++ lib/info.dart
@@ -0,0 +1,29 @@
+
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Info extends StatefulWidget {
+  const Info({super.key});
+
+  @override
+  InfoState createState() => InfoState();
+}
+
+class InfoState extends State<Info> {
+
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Center(
+
+      ),
+    )
+
+  }
+
+
+
+}
